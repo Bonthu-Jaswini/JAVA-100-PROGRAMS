e866a0325257c96055a707e9c3707f2d57bfcd45
Bonthu-Jaswini <bonthujaswinijaswini@gmail.com>
2026-09-17 11:45:31 +0530

    Create PositiveorNegativeorZero.java

---
+++ PositiveorNegativeorZero.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main{
+  public static void main(String []args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the number");
+    int n=sc.nextInt();
+    if(n>0)
+      System.out.println("Even Number");
+      else if(n<0)
+      System.out.println("Odd Number");
+      else
+      System.out.println("Zero");
+    
+  }
+}
+
+  
