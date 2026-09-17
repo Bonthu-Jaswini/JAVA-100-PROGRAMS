c76c9613c3fca4ebe74f8d1a65db9841ca304c91
Bonthu-Jaswini <bonthujaswinijaswini@gmail.com>
2026-09-17 11:57:30 +0530

    Create LargestofTwoNums.java

---
+++ LargestofTwoNums.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+  public static void main(String []args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the values of a,b");
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    if(a>b)
+      System.out.println(a+" is Largest Number");
+  }
+}
