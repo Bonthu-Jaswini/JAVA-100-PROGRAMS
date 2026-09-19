811fd841efad9d9775f82e425d02184f548298f6
Bonthu-Jaswini <bonthujaswinijaswini@gmail.com>
2026-09-19 12:00:59 +0530

    Create PrimeNumberInRange.java

---
+++ PrimeNumberInRange.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+public class PrimeNumberInRange {
+	public static void main(String []args){
+    
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter start value");
+        int start=sc.nextInt();
+	    System.out.println("Enter end value");
+        int end=sc.nextInt();
+         int count=0;
+        for(int n=start;n<=end;n++){
+        for(int i=1;i<=n;i++){
+            if(n%i==0)
+            count++;
+             }
+        if(count==2)
+            System.out.println(n+ " is a Prime Number");
+        else
+            System.out.println(n+ " is not a Prime Number");
+    }
+}
+}         
+        
+	   
