86cd9bdbbb2109fca5d27e0fd207edd482817e39
Bonthu-Jaswini <bonthujaswinijaswini@gmail.com>
2026-09-19 11:38:02 +0530

    Update ProductofDigitsinNumber.java

--- ProductofDigitsinNumber.java
+++ ProductofDigitsinNumber.java
@@ -1,6 +1,6 @@
 
 import java.util.*;
-public class RevereseTheNumber {
+public class ProductofDigitsinNumber {
 	public static void main(String []args){
 	    Scanner sc=new Scanner(System.in);
 	    System.out.println("Enter n value");
