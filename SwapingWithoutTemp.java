a9b8691f9dc18477225928f1f031d9e9b5d986b7
Bonthu-Jaswini <bonthujaswinijaswini@gmail.com>
2026-09-19 09:59:17 +0530

    Create SwapingWithoutTemp.java

---
+++ SwapingWithoutTemp.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class SwapingWithoutTemp{
+  public static void main(String []args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a,b values");
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    a=a+b;
+    b=a-b;
+    a=a-b;
+  }
+}
