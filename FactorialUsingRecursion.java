ecaee9d69ef87fc1e24fd25d57ac953502c13800
Bonthu-Jaswini <bonthujaswinijaswini@gmail.com>
2026-09-19 14:24:18 +0530

    Create FactorialUsingRecursion.java

---
+++ FactorialUsingRecursion.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+
+    static long factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}
