5f50309a480e144f551fc4d5591ec6b2f6fd92be
Bonthu-Jaswini <bonthujaswinijaswini@gmail.com>
2026-09-19 11:16:20 +0530

    Create SumofDigitsInNumber.java

---
+++ SumofDigitsInNumber.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class RevereseTheNumber {
+	public static void main(String []args){
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter n value");
+        int n=sc.nextInt();
+        int sum=0;
+            
+        while(n!=0){
+            int digit=n%10;
+            sum=sum+digit;
+            n=n/10;
+
+        }
+        System.out.println("sum of the digits in given Number is:"+sum);
+    }
+        }
+            
+        
+	   
